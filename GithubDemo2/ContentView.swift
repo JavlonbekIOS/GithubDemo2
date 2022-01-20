f59a73fd347f41dd78aa22817ebff8f71dfcd280
Javlonbek <javlonbekalhamdulillah@gmail.com>
2022-01-20 13:13:54 +0500

    Initial Commit

---
+++ GithubDemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubDemo2
+//
+//  Created by Javlonbek on 20/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubDemo2/GithubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo2App.swift
+//  GithubDemo2
+//
+//  Created by Javlonbek on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
